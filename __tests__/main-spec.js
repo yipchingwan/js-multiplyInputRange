5fa3116f1ec8e12d8e89142a23562eb8ac63b055
KYLE\Surface <kyle.public10415@gmail.com>
2018-11-21 00:58:37 +0800

    finished test case for From index bigger than To index

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -3,3 +3,7 @@ const multiplyInputRange = require('../main');
 it ('should multipy within input range with normal input range', () => {
     expect(multiplyInputRange(2, 4)).toBe('2*2=4\n2*3=6  3*3=9\n2*4=8  3*4=12  4*4=16\n');
 });
+
+it ('should return null when range From is bigger than To', () => {
+    expect(multiplyInputRange(4, 2)).toBe(null);
+});
--- main.js
+++ main.js
@@ -1,34 +1,40 @@
 function multiplyInputRange(a, b){
-    var from = a;
-    var to = b;
-    var tmpLastRowBoxLengthCount = [];
-    var tmpString="";
-    var resultString="";
-    for(var i =from; i<=to; i++){
-        tmpString = i+"*"+to+"="+i*to+"  ";
-        tmpLastRowBoxLengthCount.push(tmpString.length);
-    }
-    for(var i = from; i<=to; i++){
-        for(var j = from ; j<=to; j++){
-            if(i==j){
-                resultString += j+"*"+i+"="+j*i+"\n";
-                break;
-            }
-            else if(j==to){
-                resultString += j+"*"+i+"="+j*i;
-            }
-            else{
-                var tmp = j+"*"+i+"="+j*i+"  ";
-                var padSpace = tmpLastRowBoxLengthCount[j-from]-tmp.length;
-                for(var k=0; k<padSpace; k++){
-                    tmp+=" ";
+    if(a<b && a<=1000 && b<=1000){
+        var from = a;
+        var to = b;
+        var tmpLastRowBoxLengthCount = [];
+        var tmpString="";
+        var resultString="";
+        for(var i =from; i<=to; i++){
+            tmpString = i+"*"+to+"="+i*to+"  ";
+            tmpLastRowBoxLengthCount.push(tmpString.length);
+        }
+        for(var i = from; i<=to; i++){
+            for(var j = from ; j<=to; j++){
+                if(i==j){
+                    resultString += j+"*"+i+"="+j*i+"\n";
+                    break;
                 }
-                resultString+=tmp;
+                else if(j==to){
+                    resultString += j+"*"+i+"="+j*i;
+                }
+                else{
+                    var tmp = j+"*"+i+"="+j*i+"  ";
+                    var padSpace = tmpLastRowBoxLengthCount[j-from]-tmp.length;
+                    for(var k=0; k<padSpace; k++){
+                        tmp+=" ";
+                    }
+                    resultString+=tmp;
+                }
+                
             }
             
         }
-        
+        return resultString; 
     }
-    return resultString;
+    else if(a>b || a<1 || a>1000 || b<1 || b>1000 || a===null || b===null){
+        return null;
+    }
+    
 }
 module.exports = multiplyInputRange;
